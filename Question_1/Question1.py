9e1d18ca6775a13135af2c97711d5d031b8b8f6b
deepti <gorrepatideepti@gmail.com>
2025-03-06 17:44:19 -0500

    added in is_palindrome
    helper function to determine if a string is a palindrome. case insensitive

--- Question_1/Question1.py
+++ Question_1/Question1.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "It peed!"
+    if is_palindrome("Bocab"):
+        return "palindrome"
+    return "not palindrome"
+
+#we can iterate through strings like lists in python using a for loop
+def is_palindrome(string: str):
+    palindrome_counter = 0
+    i = 0;
+    j = len(string) - 1
+    while i < j:
+        if string[i].lower() != string[j].lower():
+            break;
+        i = i + 1
+        j = j - 1
+        palindrome_counter = palindrome_counter + 1  
+         
+    if palindrome_counter == len(string)//2:
+        return True
+    return False
