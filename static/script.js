bfd9fa718faada0cd8bb8a4f694ee88d47369153
deepti <gorrepatideepti@gmail.com>
2025-03-31 20:11:48 -0400

    edited javascript a little

--- static/script.js
+++ static/script.js
@@ -1,9 +1,9 @@
 function openForm(assessmentId) {
     var form = document.getElementById("myForm" + assessmentId);
-    form.style.display = "block";  // Open the form
+    form.style.display = "block"; 
 }
 
 function closeForm(assessmentId) {
     var form = document.getElementById("myForm" + assessmentId);
-    form.style.display = "none";  // Close the form
+    form.style.display = "none";  
 }
